c6c40d39f6c5576de7169812e7d9755e836b7eb2
hautera <hautea@uw.edu>
2017-12-08 23:35:35 -0800

    Information Update
    Removed unecessary pictures. Updated main page. Added auction info to support page.

--- uwsails/index.php
+++ uwsails/index.php
@@ -15,7 +15,7 @@
 	<div class="container">
 		<div>
 			<h3>About the team</h3>
-			<p>Welcome to the University of Washington Sailing team webpage. We are in the Northwest intercolegiate sailing association (NWICSA for short) we compete against other schools in the region, and in the nation. We currently practice every Monday, Tuesday, Wednesday, and Friday at <a href="http://www.sailsandpoint.org">Sail Sand Point</a> from 3pm until around 6pm (or unitl dark). If you're a student at UW and would like to sail: come join us! No experience necessary.</p>
+			<p>Welcome to the University of Washington Sailing team webpage. We are in the Northwest intercolegiate sailing association (NWICSA for short) we compete against other schools in the region, and in the nation. We're not practicing for the rest of the quarter. Check back in the beginning of Winter quarter for practice times! If you're a student at UW and would like to sail: come join us! No experience necessary.</p>
 			<img src="images/TeamPic3.jpg" width="500px" >
 		</div>
 	<?php include("include/footer.html"); ?>
--- uwsails/support.php
+++ uwsails/support.php
@@ -12,6 +12,8 @@
 	<?php include("include/header.html"); ?>
 	<h1>Thank you for your support!</h1>
 	<h2>There are a lot of ways to help us:</h2>
+	<h3>Team Auction:</h3>
+	<p>The team auction is our primary means of raising money. It will be held <a href="https://calendar.google.com/calendar/r/week/2018/2/23"><u><b>Feburary 23rd, 2018</u>. Save the date!! </b></a>It will be held in the <a href ="http://depts.washington.edu/ecc/">ECC building</a> on campus. Directions are <a href="https://www.google.com/maps/place/Samuel+E.+Kelly+Ethnic+Cultural+Center/@47.6552222,-122.3135293,16z/data=!4m8!1m2!2m1!1sECC!3m4!1s0x549014f249fb3b57:0x65ddba01328d0f69!8m2!3d47.655095!4d-122.314784">here</a>. There will be food, cool stuff to buy, and awesome sailors to talk to! Please be there to support us!</p>
 	<h3>Donate Money:</h3>
 	<p>We are in desperate need of money.
 	We use the money donated to travel to regattas, pay for boats, and maintain the boats we use. This helps open up the team to everyone regardless of how much money they have. We are able to send good sailors to awesome places because of the donations. Any amount of money that can be donated is greatly appreciated! Venmo @UW-Sailing money and say it's a donation.</p>
