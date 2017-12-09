3572223af6583757e29dae25483841171ea5c0ff
hautera <hautea@uw.edu>
2017-12-08 22:43:27 -0800

    Pictures!
    Pictures tab now displays all the pictures on firestore

---
+++ uwsails/include/pictures.js
@@ -0,0 +1,54 @@
+//const firebase = require("firebase");
+// Required for side-effects (according to google idk if ittll woorrrrk)
+//require("firebase/firestore");
+
+/*MIGHT NOT NEED THIS BECAUSE OF THE HEADER*/
+/*ITS NOT THROWING ERRORS SO DONT CHANGE IT :)*/
+var config = {
+    apiKey: "AIzaSyCQotoFvcv3aqsAtusH9ynOgGalusrnT9g",
+    authDomain: "husky-sailing-site.firebaseapp.com",
+    databaseURL: "https://husky-sailing-site.firebaseio.com",
+    projectId: "husky-sailing-site",
+    storageBucket: "husky-sailing-site.appspot.com",
+    messagingSenderId: "653011603129"
+  };
+firebase.initializeApp(config);
+
+//database reference :)
+var db = firebase.firestore();
+
+/**
+ * Puts image from firestorage onto page
+ * @param picPath the place the image is stored in firestore, if not correct
+ * @param albumID The name of the album to be displayed
+ * this function logs an error to console
+ */
+function display( picPath, albumID ){
+   var path = "pictures/" +picPath
+   //Instalize the variables :)
+   var storage = firebase.storage();
+   var storageRef = storage.ref();
+   var picRef = storageRef.child(path);
+   picRef.getDownloadURL().then( function( URL ){
+         document.getElementById("pic").innerHTML += "<a href='/uwsails/pictures/albums.php?album="+albumID+"'><span class='imag_container'><img src=\"" +URL+"\" class='smoll_img'><h3 class='img_caption'>"+albumID+"</h3></span></a>";
+   }).catch( function( error){
+      console.error( "Path might be invalid", error.message );
+   });
+}
+
+
+db.collection('pictures').get().then(
+   function( albums ){
+      albums.forEach( function( album ) {    //chooses first picture from album; and sends that to page for display
+      if( album != null ){
+         var pic = album.data().pics[0];
+         display( album.id + "/"+pic, album.id);
+      } else {    // error log
+         console.log( "Balls!");
+      }
+   });
+}).catch(
+   function(e) {
+      console.error("Problem retrieving data!", e.message);
+   }
+);
